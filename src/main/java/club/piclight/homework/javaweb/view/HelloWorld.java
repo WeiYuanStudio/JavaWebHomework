ea978a06bbe58dadf581d89386bfa97f1fb1727b
WeiYuanStudio <WeiYuanStudio@gmail.com>
2020-02-27 23:18:45 +0800

    :truck: move ex

--- src/main/java/club/piclight/homework/javaweb/view/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package club.piclight.homework.javaweb.view;
-
-import club.piclight.homework.javaweb.IndexUtil.IndexMark;
-
-import javax.servlet.http.HttpServlet;
-
-@IndexMark(experimentID = 1, title = "Hello World !", url = "")
-public class HelloWorld extends HttpServlet {
-}
